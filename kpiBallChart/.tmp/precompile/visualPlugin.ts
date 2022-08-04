7aad40ffaa4c1a0bcd8e8b557c8a3414443f91e2
RJHARENBERG <reinardharenberg@gmail.com>
2022-08-04 17:09:40 +0200

    start power bi visual incorporated

---
+++ kpiBallChart/.tmp/precompile/visualPlugin.ts
@@ -0,0 +1,32 @@
+import { Visual } from "../../src/visual";
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+import VisualConstructorOptions = powerbiVisualsApi.extensibility.visual.VisualConstructorOptions;
+import DialogConstructorOptions = powerbiVisualsApi.extensibility.visual.DialogConstructorOptions;
+var powerbiKey: any = "powerbi";
+var powerbi: any = window[powerbiKey];
+var kpiBallChart70A84ACCF598436F83218A182FDA408D_DEBUG: IVisualPlugin = {
+    name: 'kpiBallChart70A84ACCF598436F83218A182FDA408D_DEBUG',
+    displayName: 'KpiBallChart',
+    class: 'Visual',
+    apiVersion: '3.8.0',
+    create: (options: VisualConstructorOptions) => {
+        if (Visual) {
+            return new Visual(options);
+        }
+        throw 'Visual instance not found';
+    },
+    createModalDialog: (dialogId: string, options: DialogConstructorOptions, initialState: object) => {
+        const dialogRegistry = globalThis.dialogRegistry;
+        if (dialogId in dialogRegistry) {
+            new dialogRegistry[dialogId](options, initialState);
+        }
+    },
+    custom: true
+};
+if (typeof powerbi !== "undefined") {
+    powerbi.visuals = powerbi.visuals || {};
+    powerbi.visuals.plugins = powerbi.visuals.plugins || {};
+    powerbi.visuals.plugins["kpiBallChart70A84ACCF598436F83218A182FDA408D_DEBUG"] = kpiBallChart70A84ACCF598436F83218A182FDA408D_DEBUG;
+}
+export default kpiBallChart70A84ACCF598436F83218A182FDA408D_DEBUG;
